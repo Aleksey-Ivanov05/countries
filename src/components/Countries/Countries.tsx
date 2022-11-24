8a905acc6303e2b120cba172a0c5e617d1a37d72
Aleksey Ivanov <lepri4dw@gmail.com>
2022-11-24 16:31:19 +0600

    Получил названия всех стран
    Сделал запрос, чтобы получить названия всех стран, и вывел их на экран

--- src/components/Countries/Countries.tsx
+++ src/components/Countries/Countries.tsx
@@ -1,39 +1,24 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import Country from "./Country";
+import {CountryName} from "../../types";
+import axios from "axios";
 
 const Countries = () => {
+  const [countries, setCountries] = useState<CountryName[]>([]);
+  const fetchData = useCallback(async () => {
+    const response = await axios.get<CountryName[]>('/all?fields=alpha3Code,name');
+    setCountries(response.data);
+  }, []);
+
+  useEffect(() => {
+    fetchData().catch(console.error);
+  }, [fetchData])
+
   return (
     <div className="border border-dark p-3 overflow-auto me-5" style={{maxHeight: '400px'}}>
-      <Country name={'Kyrgyzstan'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-      <Country name={'Russia'}/>
-
+      {countries.map(country => (
+        <Country key={country.alpha3Code} name={country.name}/>
+      ))}
     </div>
   );
 };
